eb24cf874e11f04b5d2af1bab49f63f12a2c48a8
Goga Barabadze <goga.barabadze73@gmail.com>
2020-10-14 12:58:53 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/qrpong.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qrpong.dir/main.c.o"
+  "qrpong"
+  "qrpong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qrpong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/qrpong.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for qrpong.
+# This may be replaced when dependencies are built.
