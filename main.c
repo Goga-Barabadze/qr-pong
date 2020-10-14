f7bf7ce042fbda02981d1b9bf4b8db8347034d01
Goga Barabadze <goga.barabadze73@gmail.com>
2020-10-14 13:03:51 +0200

    Added Position and Dimensions

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+struct Panel{
+
+};
+
+struct Position {
+    short x;
+    short y;
+};
+
+struct Dimensions {
+    short height;
+    short width;
+};
+
 int main() {
     printf("Hello, World!\n");
     return 0;
